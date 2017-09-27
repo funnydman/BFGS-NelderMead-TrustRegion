d6a333bca667c17fa4f13f4129927a6e66df7a69
FUNNYDMAN <crazy-dzmitry@mail.ru>
2017-09-27 15:48:32 +0200

    Update nelder_mead.py

--- nelder_mead.py
+++ nelder_mead.py
@@ -25,7 +25,7 @@ class Vector(object):
         y = self.y - other.y
         return Vector(x, y)
 
-    def __mul__(self, other):
+    def __rmul__(self, other):
         x = self.x * other
         y = self.y * other
         return Vector(x, y)
@@ -38,7 +38,7 @@ class Vector(object):
     def c(self):
         return (self.x, self.y)
         
-
+# objective function
 def f(point):
     x, y = point
     return x**2 + x*y + y**2 - 6*x - 9*y
@@ -51,7 +51,6 @@ def nelder_mead(alpha=1, beta=0.5, gamma=2, maxiter=10):
     v3 = Vector(0, 1)
 
     for i in range(maxiter):
-        
         adict = {v1:f(v1.c()), v2:f(v2.c()), v3:f(v3.c())}
         points = sorted(adict.items(), key=lambda x: x[1])
         
@@ -59,10 +58,11 @@ def nelder_mead(alpha=1, beta=0.5, gamma=2, maxiter=10):
         g = points[1][0]
         w = points[2][0]
         
-        mid = (g + b)/2
         
+        mid = (g + b)/2
+
         # reflection
-        xr = mid * 2 - w
+        xr = mid + alpha * (mid - w)
         if f(xr.c()) < f(g.c()):
             w = xr
         else:
@@ -71,18 +71,22 @@ def nelder_mead(alpha=1, beta=0.5, gamma=2, maxiter=10):
             c = (w + mid)/2
             if f(c.c()) < f(w.c()):
                 w = c
-        # expansion
         if f(xr.c()) < f(b.c()):
-            xe = xr * 2 - mid
+
+            # expansion
+            xe = mid + gamma * (xr - mid)
             if f(xe.c()) < f(xr.c()):
                 w = xe
             else:
                 w = xr
-        # contraction
         if f(xr.c()) > f(g.c()):
-            xc = mid * (1 - beta) + w * beta
+            
+            # contraction
+            xc = mid + beta * (w - mid)
             if f(xc.c()) < f(w.c()):
                 w = xc
+
+        # update points
         v1 = w
         v2 = g
         v3 = b
